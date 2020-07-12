04663b846829420376a1772bc6a092a56a82bae4
Johnny Foulds <hfoulds@gmail.com>
2020-07-12 22:08:06 +0200

    Update SampleJob.scala

--- SparkSample/src/main/scala/SampleJob.scala
+++ SparkSample/src/main/scala/SampleJob.scala
@@ -6,9 +6,9 @@ object SampleJob extends App {
 		.builder()
   	.appName("Sample Job")
   	.master("yarn")
-		.config("spark.hadoop.fs.defaultFS", "hdfs://pshp111zatcwi:9000")
-		.config("spark.hadoop.yarn.resourcemanager.address", "pshp111zatcwi:8055")
-		.config("spark.yarn.archive", "hdfs://pshp111zatcwi:9000/spark/spark-libs-2.4.3.jar")
+		.config("spark.hadoop.fs.defaultFS", "hdfs://pshp111:9000")
+		.config("spark.hadoop.yarn.resourcemanager.address", "pshp111:8055")
+		.config("spark.yarn.archive", "hdfs://pshp111:9000/spark/spark-libs-2.4.3.jar")
 		.getOrCreate()
 
 	val sc = spark.sparkContext
@@ -22,7 +22,7 @@ object SampleJob extends App {
 	df.show()
 
 	/*
-	val logPath = "hdfs://pshp111zatcwi:9000/u_ex190620.log"
+	val logPath = "hdfs://pshp111:9000/u_ex190620.log"
 
 	val logTextData = sc.textFile(logPath)
 		.filter(line=> !line.startsWith("#"))
